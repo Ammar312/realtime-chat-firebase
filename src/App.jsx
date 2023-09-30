5c8aa995463cd91cc21b31bc3f3723b67f96a8aa
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-09-30 20:15:52 +0500

    vite tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <div className="text-3xl font-bold underline">REAL TIME CHAT APP </div>
+    </div>
+  );
+};
+
+export default App;
