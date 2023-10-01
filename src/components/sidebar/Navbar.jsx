7d0639a3ec64f83fa602da853c248543fa7a0b3d
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-10-01 20:59:53 +0500

    making sidebar ui

--- src/components/sidebar/Navbar.jsx
+++ src/components/sidebar/Navbar.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div className=" bg-blue-800 flex items-center justify-between gap-2 p-3">
+      <div className="text-xl text-white">Mess Chat</div>
+      <div className=" flex items-center gap-2">
+        <img src="" alt="" className="w-8 h-8 rounded-full border-2" />
+        <span>Ronald Duck</span>
+      </div>
+      <div className="p-1 border border-white bg-slate-400 text-xs">Logout</div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 const SideBar = () => {
-  return <div className="border-red-500 border w-[300px]">Sidebar</div>;
+  return (
+    <div className="border-red-500 border w-[330px] bg-blue-950">
+      <Navbar />
+    </div>
+  );
 };
 
 export default SideBar;
