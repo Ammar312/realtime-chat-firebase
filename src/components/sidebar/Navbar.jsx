6a6c00717a65c81b38c17786d3d433361aafeed9
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-10-01 21:40:51 +0500

    working on ui search

--- src/components/sidebar/Navbar.jsx
+++ src/components/sidebar/Navbar.jsx
@@ -3,9 +3,12 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className=" bg-blue-800 flex items-center justify-between gap-2 p-3">
-      <div className="text-xl text-white">Mess Chat</div>
       <div className=" flex items-center gap-2">
-        <img src="" alt="" className="w-8 h-8 rounded-full border-2" />
+        <img
+          src="https://images.pexels.com/photos/17977042/pexels-photo-17977042/free-photo-of-smiling-woman-in-purple-dress-on-vacation.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt=""
+          className="w-10 h-10 rounded-full object-cover"
+        />
         <span>Ronald Duck</span>
       </div>
       <div className="p-1 border border-white bg-slate-400 text-xs">Logout</div>
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
 
 const SideBar = () => {
   return (
     <div className="border-red-500 border w-[330px] bg-blue-950">
       <Navbar />
+      <SearchBar />
     </div>
   );
 };
