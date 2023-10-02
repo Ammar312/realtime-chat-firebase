5078462e6a3673479426bfc73ee044173ebee28b
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-10-02 16:08:59 +0500

    input bar working

--- src/components/chat/Input.jsx
+++ src/components/chat/Input.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import attach from "../../assets/attach.png";
+import img from "../../assets/img.png";
 
 const Input = () => {
   return (
-    <div>
-      <div className=" bg-white p-4">
-        input box
-        {/* <input type="text" className="" /> */}
+    <div className="overflow-hidden">
+      <div className=" bg-white p-4 flex justify-between">
+        <input type="text" className="w-full" placeholder="Type a message..." />
+        <div className="flex items-center">
+          <img src={attach} alt="" />
+          <img src={img} alt="" />
+        </div>
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Chat from "../components/chat/Chat";
 const Home = () => {
   return (
     <div className="bg-green-300 min-h-screen flex justify-center items-center">
-      <div className="border-2 border-white flex h-[510px] w-[850px]">
+      <div className="border-2 border-white flex h-[510px] w-[850px] overflow-hidden">
         <Sidebar />
         <Chat />
       </div>
