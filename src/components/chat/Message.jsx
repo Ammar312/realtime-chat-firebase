3a3ab9ac55dfd72e299877990d619babbe0b245d
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-10-04 16:16:38 +0500

    owner sude complete

--- src/components/chat/Message.jsx
+++ src/components/chat/Message.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Message = () => {
+  const [owner, setOwner] = useState(true);
+
   return (
-    <div className="flex mb-4">
+    <div className={owner ? "flex flex-row-reverse mb-4 gap-2" : "flex mb-4"}>
       <div className="flex flex-col">
         <img
           src="https://images.pexels.com/photos/13193108/pexels-photo-13193108.jpeg?auto=compress&cs=tinysrgb&w=600"
@@ -11,8 +13,21 @@ const Message = () => {
         />
         <span className="text-xs text-slate-500">Just now</span>
       </div>
-      <div className="max-w-[80%] flex flex-col gap-2">
-        <p className="bg-white p-2">Hey Dy!</p>
+      <div
+        className={`max-w-[80%] flex flex-col gap-2 ${owner && "items-end"}`}
+      >
+        <p
+          className={`bg-white p-2 max-w-max ${
+            owner ? "rounded-tl-xl bg-blue-300 text-white" : "rounded-tr-xl"
+          }  rounded-br-xl rounded-bl-xl`}
+        >
+          Hey Dy!
+        </p>
+        <img
+          src="https://images.pexels.com/photos/17720157/pexels-photo-17720157/free-photo-of-wood-landscape-water-summer.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+          alt=""
+          className="w-[50%]"
+        />
       </div>
     </div>
   );
--- src/components/chat/Messages.jsx
+++ src/components/chat/Messages.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Message from "./Message";
 
 const Messages = () => {
+  const [owner, setOwner] = useState(true);
   return (
-    <div className=" bg-purple-100 h-[385px] overflow-y-auto ">
+    <div
+      className={`bg-purple-100 h-[385px] overflow-y-auto p-2 flex flex-col ${
+        owner && "items-end"
+      }`}
+    >
       <Message />
       <Message />
       <Message />
